745839b762a953568bf94a62fbdff538aa90789c
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-05-04 21:02:33 -0500

    Update model

--- src/violajones/AdaBoost.py
+++ src/violajones/AdaBoost.py
@@ -1,13 +1,23 @@
-"""Perfoem Modified Adaboost Algorithm in Viola Jones Face Detection."""
+"""Perform Modified Adaboost Algorithm in Viola Jones Face Detection."""
 
 
 import numpy as np
+from tqdm import tqdm
+from functools import partial
+from violajones.HaarFeature import HaarFeature
 # from functools import partial
 # import numpy as np
 # from violajones.HaarLikeFeature import HaarLikeFeature
 # from violajones.HaarLikeFeature import FeatureTypes
 # import progressbar
 # from multiprocessing import Pool
+
+
+FEATURE_TYPE = {'type-2-y': (1, 2),
+                'type-2-x': (2, 1),
+                'type-3-y': (3, 1),
+                'type-3-x': (1, 3),
+                'type-4': (2, 2)}
 
 
 def AdaBoost(images, groundtruth_labels, num_pos, num_neg, feature_size=0):
@@ -32,7 +42,7 @@ def AdaBoost(images, groundtruth_labels, num_pos, num_neg, feature_size=0):
             only 0 and 1.
         num_pos (int): number of positive image samples
         num_neg (int): number of negative image samples
-
+        feature_size (int): size of features
 
     :param num_classifiers: Number of classifiers to select, -1 will use all
     classifiers
@@ -42,6 +52,9 @@ def AdaBoost(images, groundtruth_labels, num_pos, num_neg, feature_size=0):
 
 
     """
+    # ----------------------------------------------------------------------- #
+    # Parameters Settings
+
     # number of all the images
     num_images = num_pos + num_neg
 
@@ -54,19 +67,73 @@ def AdaBoost(images, groundtruth_labels, num_pos, num_neg, feature_size=0):
     feature_width = default_size if feature_size == 0 else img_width
 
     # Create initial weights and labels
-    weight_pos = np.ones(num_pos) * 1. / (2 * num_pos)
-    weight_neg = np.ones(num_neg) * 1. / (2 * num_neg)
+    weight_pos = np.ones(num_pos).astype(np.float32) / (2 * num_pos)
+    weight_neg = np.ones(num_neg).astype(np.float32) / (2 * num_neg)
     weights = np.hstack((weight_pos, weight_neg))
     labels = np.hstack((np.ones(num_pos), np.zeros(num_neg)))
 
+    # ----------------------------------------------------------------------- #
+    # Generate features
+
     # Create features for all sizes and locations
-    pass
+    features = feature_generate(img_height, img_width, feature_height, feature_width)
+    num_features = len(features)
+    print("[*] Generated " + str(num_features) + "features!")
+
+    votes = np.zeros((num_images, num_features))
+    for idx in tqdm(range(num_images)):
+        votes[idx, :] = np.array(list(map(partial(get_delta, image=images[idx]), features)))
 
 
-def feature_generate():
-    """Features generation for Viola Jones Face Detection.
+    # ----------------------------------------------------------------------- #
+
+
+
+
+    # ----------------------------------------------------------------------- #
+
+
+
+    # ----------------------------------------------------------------------- #
+
+
+def feature_generate(img_height, img_width, feature_height, feature_width):
+    """Features generation.
 
     Generate features for all 5 type, all feature size, all training images
 
+    Args:
+
+
+    Returns:
+
     """
-    pass
+    features = []
+    print("[*] Generate features...")
+    for feature in FEATURE_TYPE:
+        # default width
+        width = FEATURE_TYPE[feature][0]
+
+        for filter_width in tqdm(range(width, feature_width, width)):
+            # default height
+            height = FEATURE_TYPE[feature][1]
+
+            for filter_height in tqdm(range(height, feature_height, height)):
+                for i in range(img_width - filter_width):
+                    for j in range(img_height - filter_height):
+                        # negative examples
+                        features.append(HaarFeature(feature, (i, j),
+                                                    filter_width,
+                                                    filter_height, 0.05, 0))
+
+                        # positive examples
+                        features.append(HaarFeature(feature, (i, j),
+                                                    filter_width,
+                                                    filter_height, 0.05, 1))
+
+    return features
+
+
+def get_delta(feature, image):
+    """Get delta from those features."""
+    return feature._get_delta(image)
